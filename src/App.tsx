0e715e75b7c9a8b56210c59e7cc5fdcc97201760
Ike Wilson <iwilson@ecfmg.org>
2021-02-25 23:59:28 -0500

    parse country data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Button} from 'antd';
 import './App.css';
+import axios from 'axios';
+import {CountryInfo} from './interfaces'
 
 function App() {
   return (
@@ -18,9 +21,27 @@ function App() {
         >
           Learn React
         </a>
+        <Button onClick={() => get()}>Click</Button>
       </header>
     </div>
   );
+}
+
+async function get() {
+  var result: CountryInfo = await axios.get('https://restcountries.eu/rest/v2/alpha/col')
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    return response.data as CountryInfo
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+    var empty = {} as CountryInfo;
+    return empty;
+  });
+
+  console.log(result);
 }
 
 export default App;
